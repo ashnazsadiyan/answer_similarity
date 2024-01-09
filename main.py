abbe5e09032ed8a793b3b9ed3e6d1576e4e6202f
Tuwan Ashnaz Sadiyan <ashnassadiyan@gmail.com>
2024-01-09 09:56:40 +0530

    test

--- main.py
+++ main.py
@@ -19,10 +19,18 @@ def index():
 
 # get score
 @app.post("/answer")
-def get_score(similaranswer: SimilarAnswer):
-    model = SentenceTransformer('all-MiniLM-L6-v2')
-    embeddings = model.encode([similaranswer.expected_answer, similaranswer.given_answer], convert_to_tensor=True)
-    cosine_score = util.pytorch_cos_sim(embeddings[0], embeddings[1])
-    similarity_score = cosine_score.item()
-    print(f"Similarity Score for Answers: {similarity_score}")
-    return similarity_score
+def get_score(similaranswer: SimilarAnswer, response: Response):
+    try:
+        model = SentenceTransformer('all-MiniLM-L6-v2')
+        embeddings = model.encode([similaranswer.expected_answer, similaranswer.given_answer], convert_to_tensor=True)
+        cosine_score = util.pytorch_cos_sim(embeddings[0], embeddings[1])
+        similarity_score = cosine_score.item()
+        print(f"Similarity Score for Answers: {similarity_score}")
+        return {"Similarity Score for Answers":similarity_score}
+    except Exception as e:
+        print(e)
+        response.status_code = 500
+        return {"message": "something went wrong"}
+
+
+
